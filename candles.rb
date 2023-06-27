ab1beb741b69aa28397346bbcef606d23eb66839
Jared Harvick <jharvick38@gmail.com>
2023-06-26 19:46:04 -0500

    candles problem

---
+++ candles.rb
@@ -0,0 +1,17 @@
+candles = [3, 2, 1, 3]
+index = 0
+tall_candle = 0
+count = 0
+
+while index < candles.length
+  if candles[index] > tall_candle
+    tall_candle = candles[index]
+    count = 1
+  end
+  if candles[index] == tall_candle
+    count += 1
+  end
+  index += 1
+end
+
+p count
