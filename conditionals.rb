52b1f990955980c7089f9a521bc1536c3ed11895
Jared Harvick <jharvick38@gmail.com>
2023-07-06 18:55:34 -0500

    conditionals practice

---
+++ conditionals.rb
@@ -0,0 +1,21 @@
+# 1. Use a variable to store a number, then write a condition that prints 0 if the number is equal to 10, and prints -1 otherwise.
+
+# x = 10
+
+# if x == 10
+#   p 0
+# else
+#   p -1
+# end
+
+# 8. Use a variable to store a number, then write a condition that prints 100 if the number equals 100, prints 99 if the number is equal to 99, and prints 0 otherwise.
+
+x = 90
+
+if x == 100
+  p 100
+elsif x == 99
+  p 99
+else
+  p 0
+end
