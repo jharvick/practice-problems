9c96bff7daf71dcdc3bba54b21b6d24e8a570b2c
Jared Harvick <jharvick38@gmail.com>
2023-07-06 19:27:40 -0500

    arrays practice problems

---
+++ arrays.rb
@@ -0,0 +1,9 @@
+# 3. Create an array to store 5 numbers. Then print out each number on separate lines with a while loop.
+
+numbers = [2, 4, 6, 8, 10]
+index = 0
+
+while index < numbers.length
+  p numbers[index]
+  index += 1
+end
