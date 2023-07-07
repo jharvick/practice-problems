f2e918d45dbae59983060a45ecaee7e4f5db3ea4
Jared Harvick <jharvick38@gmail.com>
2023-07-06 19:11:38 -0500

    loops practice problems

---
+++ loops.rb
@@ -0,0 +1,29 @@
+# 3. Write a while loop that asks the user to enter a word and will run forever until the user enters the word "stop".
+
+# while true
+#   puts "Enter a word: "
+#   input = gets.chomp
+#   if input == "stop"
+#     break
+#   end
+# end
+
+# 4. Write a while loop that prints the numbers 0 through 100, increasing by 5 each time.
+
+# x = 0
+
+# while x < 105
+#   p x
+#   x += 5
+# end
+
+# 10. Write a while loop that prints the even numbers from 2 to 40.
+
+x = 2
+
+while x < 41
+  if x % 2 == 0
+    p x
+  end
+  x += 1
+end
