94f20717fc03943ccf567668c9bca3e179600b5c
Jared Harvick <jharvick38@gmail.com>
2023-08-21 17:54:10 -0500

    upcase in an array

--- arrays.rb
+++ arrays.rb
@@ -1,9 +1,16 @@
 # 3. Create an array to store 5 numbers. Then print out each number on separate lines with a while loop.
 
-numbers = [2, 4, 6, 8, 10]
-index = 0
+# numbers = [2, 4, 6, 8, 10]
+# index = 0
 
-while index < numbers.length
-  p numbers[index]
-  index += 1
-end
+# while index < numbers.length
+#   p numbers[index]
+#   index += 1
+# end
+
+# 5. Create an array to store 3 strings with lower case letters. Then change the third string to have all capital letters and print the array on one line.
+
+rebels = ["ezra", "kannan", "chopper"]
+
+rebels[2] = rebels[2].upcase
+p rebels
