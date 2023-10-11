a06066fb192e47e9aaaf152b6f20653e633687fd
Jared Harvick <jharvick38@gmail.com>
2023-10-11 18:23:38 -0500

    more function practice

--- functions.rb
+++ functions.rb
@@ -1,7 +1,15 @@
 # 1. Write a function that takes in a number and returns the number times two. Then run the function and print the result.
 
-def double_number(number)
-  return number * 2
+# def double_number(number)
+#   return number * 2
+# end
+
+# p double_number(22)
+
+# 7. Write a function that takes in a number and returns the number as a string. Then run the function and print the result.
+
+def make_string(number)
+  return number.to_s
 end
 
-p double_number(22)
+p make_string(9)
