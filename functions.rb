4234f8874458a233c40815dd7b475a1597a6224b
Jared Harvick <jharvick38@gmail.com>
2023-10-11 18:35:13 -0500

    talkin bout function practice

--- functions.rb
+++ functions.rb
@@ -8,8 +8,16 @@
 
 # 7. Write a function that takes in a number and returns the number as a string. Then run the function and print the result.
 
-def make_string(number)
-  return number.to_s
+# def make_string(number)
+#   return number.to_s
+# end
+
+# p make_string(9)
+
+# 8. Write a function that takes in a string and returns the string repeated 5 times. Then run the function and print the result.
+
+def repeat_word(string)
+  return string * 5
 end
 
-p make_string(9)
+p repeat_word("pizza")
