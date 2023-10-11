f908a4252f669a2c64c4f0bc08589e89d77427f1
Jared Harvick <jharvick38@gmail.com>
2023-10-11 18:09:08 -0500

    hashing it out

--- hashes.rb
+++ hashes.rb
@@ -1,7 +1,13 @@
 # 1. Make a hash to store a person's first name, last name, and email address. Then print each attribute on separate lines.
 
-person = { "first_name" => "Jared", "last_name" => "Harvick", "email" => "jared.harvick@email.com" }
+# person = { "first_name" => "Jared", "last_name" => "Harvick", "email" => "jared.harvick@email.com" }
 
-p person["first_name"]
-p person["last_name"]
-p person["email"]
+# p person["first_name"]
+# p person["last_name"]
+# p person["email"]
+
+# 2. Make an array of hashes to store the first name and last name for 3 different people. Then print out the first person's info.
+
+person = [{ "first_name" => "Jared", "last_name" => "Harvick", "email" => "jared.harvick@email.com" }, { "first_name" => "Chloe", "last_name" => "Harvick", "email" => "chloe.harvick@email.com" }, { "first_name" => "Black", "last_name" => "Beard", "email" => "black.beard@email.com" }]
+
+p person[0]
